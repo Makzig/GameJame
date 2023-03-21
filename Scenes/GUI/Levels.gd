e26e146bb3a24513abfc047e6430b10eb43be7d0
ndrewdev <ndrew.games.1@gmail.com>
2023-03-21 22:03:23 +0300

    еще комит
    123

--- Scenes/GUI/Levels.gd
+++ Scenes/GUI/Levels.gd
@@ -1,6 +1,12 @@
 extends Control
 
+var advices = [
+	"совет №1",
+	"совет №2",
+	"совет №3"]
+
 func _ready():
+	$advice.text = advices[randi_range(0,2)]
 	$switchRect/switch.play_backwards("switch")
 	await get_tree().create_timer(0.5).timeout
 	$switchRect.visible = false
--- Scenes/GUI/MainTheme.gd
+++ Scenes/GUI/MainTheme.gd
@@ -12,12 +12,19 @@ func _on_button_play_pressed():
 	await get_tree().create_timer(0.5).timeout
 	get_tree().change_scene_to_file("res://Scenes/GUI/Levels.tscn")
 
-func anim_grab() -> void:
-	var tween = create_tween().set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_OUT)
-	$ButtonPlay.scale = Vector2.ONE 
-	tween.tween_property($ButtonPlay, "scale", Vector2.ONE * 1.1, 0.3)
+func anim_grab(v) -> void:
+	if v == 1:
+		var tween = create_tween().set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_OUT)
+		$ButtonPlay.scale = Vector2.ONE 
+		tween.tween_property($ButtonPlay, "scale", Vector2.ONE * 1.1, 0.3)
+	else:
+		var tween = create_tween().set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_OUT)
+		$ButtonPlay.scale = Vector2(1.1,1.1) 
+		tween.tween_property($ButtonPlay, "scale", Vector2.ONE, 0.3)
 
 func _on_button_play_mouse_entered():
-	anim_grab()
+	anim_grab(1)
 func _on_button_play_mouse_exited():
-	anim_grab()
+	anim_grab(2)
+
+
