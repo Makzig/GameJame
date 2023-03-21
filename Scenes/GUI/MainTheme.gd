7cc935470465f12165cfdbd573f2e14c9725f21a
ndrewdev <ndrew.games.1@gmail.com>
2023-03-21 20:35:05 +0300

    аэм
    ниче почти не изменил

--- Scenes/GUI/MainTheme.gd
+++ Scenes/GUI/MainTheme.gd
@@ -4,7 +4,7 @@ func _ready():
 	$switchRect/switch.play_backwards("switch")
 	await get_tree().create_timer(0.5).timeout
 	$switchRect.visible = false
-	$ButtonPlay/pulse.play("pulse")
+#	$ButtonPlay/pulse.play("pulse")
 
 func _on_button_play_pressed():
 	$switchRect.visible = true
@@ -13,14 +13,11 @@ func _on_button_play_pressed():
 	get_tree().change_scene_to_file("res://Scenes/GUI/Levels.tscn")
 
 func anim_grab() -> void:
-	$ButtonPlay/pulse.stop()
 	var tween = create_tween().set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_OUT)
 	$ButtonPlay.scale = Vector2.ONE 
-	tween.tween_property($ButtonPlay, "scale", Vector2.ONE * 1.5, 1)
-
-
-
-
+	tween.tween_property($ButtonPlay, "scale", Vector2.ONE * 1.1, 0.3)
 
 func _on_button_play_mouse_entered():
+	anim_grab()
+func _on_button_play_mouse_exited():
 	anim_grab()
