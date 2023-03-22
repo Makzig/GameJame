e3ada4e6561930d9ce93d370b85cf7ea4129e854
ndrewdev <ndrew.games.1@gmail.com>
2023-03-22 15:58:25 +0300

    220323

--- Scenes/GUI/Levels.gd
+++ Scenes/GUI/Levels.gd
@@ -4,13 +4,37 @@ var advices = [
 	"совет №1",
 	"совет №2",
 	"совет №3"]
+#@onready var buttons = [$list.get_children()]	#можно проще
+var buttons = ["lvl1", "lvl2", "lvl3", "lvl4", "lvl5"]
 var save:SaveResource
 
-
-
-
 func _ready():
+	
+	for button in buttons:
+		$list.get_node(button).pressed.connect(_on_pressed)
+	
 	$advice.text = advices[randi_range(0,2)]
 	$switchRect/switch.play_backwards("switch")
 	await get_tree().create_timer(0.5).timeout
 	$switchRect.visible = false
+
+func _on_button_back_pressed():
+	change_scene("res://Scenes/GUI/MainTheme.tscn")
+func _on_pressed() -> void:
+	if $list/lvl1.button_pressed:
+		change_scene("res://Scenes/Levels/1_level.tscn")
+	if $list/lvl2.button_pressed:
+		change_scene("res://Scenes/Levels/2_level.tscn")
+	if $list/lvl3.button_pressed:
+		pass
+	if $list/lvl4.button_pressed:
+		pass
+	if $list/lvl5.button_pressed:
+		pass
+
+func change_scene(path):
+	var tween = create_tween()
+	$switchRect.visible = true
+	tween.tween_property($switchRect, "color", Color(1,1,1,1), 0.5)
+	await tween.finished
+	get_tree().change_scene_to_file(path)
--- Scenes/GUI/MainTheme.gd
+++ Scenes/GUI/MainTheme.gd
@@ -1,16 +1,16 @@
 extends Control
 
+
 func _ready():
 	$switchRect/switch.play_backwards("switch")
 	await get_tree().create_timer(0.5).timeout
 	$switchRect.visible = false
 #	$ButtonPlay/pulse.play("pulse")
 
+
 func _on_button_play_pressed():
-	$switchRect.visible = true
-	$switchRect/switch.play("switch")
-	await get_tree().create_timer(0.5).timeout
-	get_tree().change_scene_to_file("res://Scenes/GUI/Levels.tscn")
+	change_scene("res://Scenes/GUI/Levels.tscn")
+
 
 func anim_grab(forward:bool = false) -> void:
 	var tween = create_tween().set_trans(Tween.TRANS_ELASTIC).set_ease(Tween.EASE_OUT)
@@ -24,6 +24,7 @@ func anim_grab(forward:bool = false) -> void:
 		$ButtonPlay.scale = need_scale 
 		tween.parallel().tween_property($ButtonPlay, "scale", origin_scale,  time)
 
+
 func _on_button_play_mouse_entered():
 	anim_grab()
 
@@ -31,3 +32,9 @@ func _on_button_play_mouse_exited():
 	anim_grab(true)
 
 
+func change_scene(path):
+	var tween = create_tween()
+	$switchRect.visible = true
+	tween.tween_property($switchRect, "color", Color(1,1,1,1), 0.5)
+	await tween.finished
+	get_tree().change_scene_to_file(path)
